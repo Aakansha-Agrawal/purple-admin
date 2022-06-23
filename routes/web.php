72e635d7dcbc5e587a393d599ba762cc7e8d6ea3
aakansha <aakanshaagrawal60@gmail.com>
2022-06-23 13:52:05 +0530

    fixed error

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/products/{id}/view',[ProductController::class, 'show']);
 Route::get('/products/{id}/delete',[ProductController::class, 'destroy']);
 Route::get('/products/{id}/restore',[ProductController::class, 'restore']);
 Route::get('/products/trash',[ProductController::class, 'deleted_data']);
-Route::get('/{file}',[ProductController::class, 'download']);
+Route::get('/{file}/download',[ProductController::class, 'download']);
 
 // --------- services routes ---------- //
 Route::get('/services',[ServiceController::class, 'index']);
