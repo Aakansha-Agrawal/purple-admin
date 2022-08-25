54204893d5945741bfa61ddbe2febaa79fd5aa9e
aakansha <aakanshaagrawal60@gmail.com>
2022-08-25 17:06:32 +0530

    reolved error with cart controller

--- app/Http/Controllers/API/ApiCartController.php
+++ app/Http/Controllers/API/ApiCartController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use Exception;
 use Illuminate\Http\Request;
